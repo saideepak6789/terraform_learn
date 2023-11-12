ce12d68673a7ec3d2194fdea9448564c31c228af
deepak <deepaksai678@gmail.com>
2023-11-13 00:24:56 +0530

    sg

---
+++ security_group.tf
@@ -0,0 +1,30 @@
+resource "aws_security_group" "my-app-sg" {
+  name        = "dev-sg"
+  description = "Allow ssh inbound traffic"
+  vpc_id      = aws_vpc.development_vpc.id
+
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [var.myip]
+  }
+  ingress {
+    description      = "SSH"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.env_prefix}-dev-sg"
+  }
+}
