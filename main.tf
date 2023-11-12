50ab37c25877b0721e53a39a4feb16870a600eaf
deepak <deepaksai678@gmail.com>
2023-11-12 19:59:57 +0530

    6th

--- main.tf
+++ main.tf
@@ -1,9 +1,16 @@
 
 
-variable "cidr_blocks"{  }
+variable "vpc_cidr_block" {  
 
-variable "environment" {
 }
+variable "subnet_cidr_block"{  
+
+}
+variable "availability_zone"{
+
+}
+
+variable "env_prefix" {}
 
 resource "aws_vpc" "development_vpc" {
   cidr_block = 
@@ -14,17 +21,17 @@ resource "aws_vpc" "development_vpc" {
 
 resource "aws_subnet" "dev_subnet-1"{
     vpc_id = aws_vpc.development_vpc.id
-    cidr_block = 
-    availability_zone = 
+    cidr_block = var.vpc_cidr_block
+    availability_zone = var.availability_zone
     tags = {
-        Name = 
+        Name = "${var.env_prefix}-vpc"
     }
 } 
 resource "aws_subnet" "dev_subnet-2"{
     vpc_id = aws_vpc.development_vpc.id
-    cidr_block = 
-    availability_zone = 
+    cidr_block = var.subnet_cidr_block
+    availability_zone = var.availability_zone
     tags = {
-        Name = 
+        Name = "${var.env_prefix}-subnet-1"
     }
 } 
